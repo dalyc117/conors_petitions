6c159a6fd1e28c89953cad4c531fd5c5c17c5c1a
Conor Daly <dalyconor1@gmail.com>
2024-11-15 14:30:20 +0000

    Dockerfile added

--- src/main/java/com/example/conors_petitions/ConorsPetitionsApplication.java
+++ src/main/java/com/example/conors_petitions/ConorsPetitionsApplication.java
@@ -2,13 +2,24 @@ package com.example.conors_petitions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ConorsPetitionsApplication {
+	@RequestMapping("/")
+	public String hello1() {
+		return "Hello";
+	}
+
+	@RequestMapping("/hello")
+	public String hello2() {
+		return "Hello Again";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConorsPetitionsApplication.class, args);
-		System.out.println("Hello World!");
 	}
 
 }
