dee4d8a3b2a0a7c399d28d13f836323cf7439799
Conor Daly <dalyconor1@gmail.com>
2024-11-19 13:36:59 +0000

    Created basic pages for some functions

--- src/main/java/com/example/conors_petitions/ConorsPetitionsApplication.java
+++ src/main/java/com/example/conors_petitions/ConorsPetitionsApplication.java
@@ -3,9 +3,8 @@ package com.example.conors_petitions;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +17,7 @@ public class ConorsPetitionsApplication {
 		return "<a href=\"/conors_petitions/listPetitions\">Click here to List!</a>";
 	}
 
-	static List<Object> petitions = new ArrayList<>();
+	static List<Petition> petitions = new ArrayList<>();
 
 	@RequestMapping("/listPetitions")
 	public String listPetitions() {
@@ -29,12 +28,58 @@ public class ConorsPetitionsApplication {
 	public String test() {
 		return "This is a very very important test";
 	}
+
+	@RequestMapping("/viewPetition")
+	public String getNew(@RequestParam int id) {
+		Petition displayPetition= null;
+		for (Petition petition : petitions) {
+			if (petition.getId() == id) {
+				displayPetition = petition;
+				break;
+			}
+		}
+
+		return "<h1>Petition info: " + displayPetition + "</h1>";
+	}
+
+	@GetMapping("/createPetitionForm")
+	@ResponseBody
+	public String getNew(@RequestParam String id) {
+		return """
+				<form action="/createPetition">
+					<label for="petition_name">Name of Petition:</label><br>
+					<input type="text" id="petition_name" name="petition_name"><br>
+					<label for="user_name">User name:</label><br>
+					<input type="text" id="user_name" name="user_name"><br><br>
+					<input type="submit" value="Create Petition">
+				</form>\s
+		""" ;
+	}
+
+	@RequestMapping("/createPetition")
+	public String newPetition(@RequestParam int id) {
+		Petition displayPetition= null;
+		for (Petition petition : petitions) {
+			if (petition.getId() == id) {
+				displayPetition = petition;
+				break;
+			}
+		}
+
+		return "Petition Created";
+	}
+
+	@PostMapping("/posting")
+	public String po(@RequestParam("someName") String someName) {
+		return "hello" + someName;
+	}
+
 	@PostConstruct
 	public void initialise() {
 		String[] users = {"Conor", "Conor2"};
-		Petition petition1 = new Petition("Down With Sheep", users);
+		Petition petition1 = new Petition("Down With Sheep", users, 1);
 		petitions.add(petition1);
-		Petition petition2 = new Petition("Up With Sheep", users);
+		Petition petition2 = new Petition("Up With Sheep", users, 2);
 		petitions.add(petition2);
 	}
 
--- src/main/java/com/example/conors_petitions/Petition.java
+++ src/main/java/com/example/conors_petitions/Petition.java
@@ -3,18 +3,24 @@ package com.example.conors_petitions;
 public class Petition {
     String name;
     String[] users;
+    int id;
 
     public Petition() {
         this.name = "No name given";
         this.users = null;
+        this.id = 0;
     }
 
-    public Petition(String name, String[] users) {
+    public Petition(String name, String[] users, int id) {
         this.name = name;
         this.users = users;
+        this.id = id;
     }
 
     public String toString() {
-        return name + ",/t" + users.length + "/n";
+        return name + "  ,  " + users.length + "  ;  ";
+    }
+    public int getId() {
+        return this.id;
     }
 }
